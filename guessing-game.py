9cb45b312dff7cffc5bde07eb66a599c1c6f0163
M-Mbugua <mbuguamichelle9@gmail.com>
2023-03-05 17:46:17 +0300

    Modified the game

--- guessing-game.py
+++ guessing-game.py
@@ -1,51 +1,53 @@
 # Python 3.10.1
 # Encoding UTF-8
-# Guessing game where a random number is generated and the player has three
-# tries to guess. If they do not, they lose and the game ends.
+# Guessing game where a random number is generated and the player has five
+# tries to guess the random number.
 
 import random
 
 
 # Generate a random number
-random_number = random.randint(0, 9)
+random_number = random.randint(0, 50)
 # Ensure that the number generated is constant throughout the game
 secret_number = random_number
 
 # for loop
-for attempts in range(3):
-    # Cast guess to integer because the input takes string data type
-    guess = int(input("Please type your lucky number: "))
-
-    # Correct guess condition
-    if guess == secret_number:
-        print("Congratulations!!! You win.")
-        break
-
-    # Wrong guess condition
-    elif attempts >= 0:
-        print("Please try again")
+# for attempts in range(5):
+#     # Cast guess to integer because the input takes string data type
+#     guess = int(input("Please enter a number between 0 and 50: "))
+#
+#     if guess == secret_number:
+#         print("Congratulations!!! You guessed correctly.")
+#         break
+#
+#     elif guess > secret_number:
+#         print("Sorry, the number you guessed is too high.\nPlease try again. ")
+#
+#     elif guess < secret_number:
+#         print("Sorry, the number you guessed is too low.\nPlease try again. ")
 
 # Tries have been exhausted
-else:
-    print(f"You lose. The secret number is {secret_number}.")
+# else:
+#     print("You're out of tries. The number was", secret_number, ".\nBetter luck next time!")
 
 
 # while loop
-# attempts = 0
-# while attempts < 3:
-#     # Cast guess to integer because the input takes string data type
-#     guess = int(input("Please type your lucky number: "))
-#     attempts = attempts + 1
-#
-#     # Correct guess condition
-#     if guess == secret_number:
-#         print("Congratulations!!! You win.")
-#         break
-#
-#     # Wrong guess condition
-#     elif attempts >= 0:
-#         print("Please try again")
-#
-# # Tries have been exhausted
-# else:
-#     print(f"You lose. The secret number is {secret_number}.")
+attempts = 0
+while attempts < 5:
+    # Cast guess to integer because the input takes string data type
+    guess = int(input("Please enter a number between 0 and 50: "))
+    attempts = attempts + 1
+
+    if guess == secret_number:
+        print("Congratulations!!! You guessed correctly.")
+        break
+
+    elif guess > secret_number:
+        print("Sorry, the number you guessed is too high.\nPlease try again. ")
+
+    elif guess < secret_number:
+        print("Sorry, the number you guessed is too low.\nPlease try again. ")
+
+# Tries have been exhausted
+else:
+    print("You're out of tries. The number was", secret_number, ".\nBetter luck next time!")
