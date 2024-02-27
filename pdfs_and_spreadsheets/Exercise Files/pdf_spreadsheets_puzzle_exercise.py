beb5b5422495f5f948637325d0275cb399d648c7
M-Mbugua <mbuguamichelle9@gmail.com>
2024-02-27 00:48:40 +0000

    Initial commit

---
+++ pdfs_and_spreadsheets/Exercise Files/pdf_spreadsheets_puzzle_exercise.py
@@ -0,0 +1,8 @@
+# Task One: Grab the Google Drive Link from .csv File
+import PyPDF2
+
+
+
+
+# Task Two: Download the PDF from the Google Drive link and
+# find the phone number that is in the document.
